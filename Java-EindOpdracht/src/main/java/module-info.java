4e0ee3cceff969e3cca307f185ad743043d2e8f4
J-Blijenburg <jensblijenburg@gmail.com>
2022-10-13 16:48:09 +0200

    dsaq

---
+++ Java-EindOpdracht/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javaeindopdracht {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.javaeindopdracht to javafx.fxml;
+    exports com.example.javaeindopdracht;
+}
