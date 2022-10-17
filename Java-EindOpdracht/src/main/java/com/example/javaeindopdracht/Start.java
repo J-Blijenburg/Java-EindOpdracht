9c43a0ee88fbe81987619a1db0ee9d02d0bf63ab
J-Blijenburg <jensblijenburg@gmail.com>
2022-10-17 20:35:56 +0200

    17-10

--- Java-EindOpdracht/src/main/java/com/example/javaeindopdracht/Start.java
+++ Java-EindOpdracht/src/main/java/com/example/javaeindopdracht/Start.java
@@ -3,6 +3,7 @@ package com.example.javaeindopdracht;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,9 @@ public class Start extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Start.class.getResource("Login-View.fxml"));
+        Image image = new Image("..\\..\\..\\Images\\Icon.jpg");
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        stage.getIcons().add(image);
         stage.setTitle("Login");
         stage.setResizable(false);
         stage.setScene(scene);
